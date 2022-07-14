629657d3e901589d0982eff4f605623fb587f2cf
Shahrukh Alam <shahrukh@swapcard.com>
2022-07-14 13:03:18 +0530

    Adds GoogleMaps xcFrameworks

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "MapsIndoorsPackage",
-            targets: ["MapsIndoors"]),
+            targets: ["MapsIndoors", "GoogleMaps", "GoogleMapsBase", "GoogleMapsCore", "GoogleMapsM4B"]),
     ],
     dependencies: [
     ],
@@ -17,6 +17,22 @@ let package = Package(
             name: "MapsIndoors",
             url: "https://github.com/MapsIndoors/MapsIndoorsIOS/releases/download/3.40.0/MapsIndoors.xcframework.zip",
             checksum: "3557bf9ac63c2942e0b6f0b4ecbc6c1ab549e511e69379875740b57dcf0ff070"
+        ),
+        .binaryTarget(
+            name: "GoogleMaps",
+            path: "Frameworks/GoogleMaps-7.0.0-beta/GoogleMaps.xcframework"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsBase",
+            path: "Frameworks/GoogleMaps-7.0.0-beta/GoogleMapsBase.xcframework"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsCore",
+            path: "Frameworks/GoogleMaps-7.0.0-beta/GoogleMapsCore.xcframework"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsM4B",
+            path: "Frameworks/GoogleMaps-7.0.0-beta/GoogleMapsM4B.xcframework"
         )
     ]
 )
